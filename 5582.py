0aad4cd510ab9706aab4af7507de86c8c2c611b6
yujinkimkimkim <105366292+yujinkimkimkim@users.noreply.github.com>
2023-07-29 12:28:27 +0900

    Update 5582.py

--- 5582.py
+++ 5582.py
@@ -1,5 +1,19 @@
-a = input()
-print(type(a))
-print(a)
-print(list(a))
-print(a[1])
+import sys
+input = sys.stdin.readline
+
+Aarr = list(input().strip())
+Barr = list(input().strip())
+
+aLen = len(Aarr)
+bLen = len(Barr)
+
+dp = [[0 for _ in range(bLen+1)]for _ in range(aLen+1)]
+ans = 0
+
+for i in range(aLen):
+    for j in range(bLen):
+        if Aarr[i] == Barr[j]:
+            dp[i+1][j+1] = dp[i][j] + 1
+            ans = max(dp[i+1][j+1], ans)
+
+print(ans)
